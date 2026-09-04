747c2d560ddf42adc31e6ccc409a8eb0e0bd3494
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2026-09-04 11:10:29 -0400

    Create drizzle.local.config.ts

--- drizzle.local.config.ts
+++ drizzle.local.config.ts
@@ -1,7 +1,10 @@
 import { config } from 'dotenv';
 import { defineConfig } from "drizzle-kit";
 
-config({ path: '.env' });
+config({ 
+  path: ['.env.local'],
+  override: true,
+});
 
 export default defineConfig({
   schema: "./src/schema.ts",
